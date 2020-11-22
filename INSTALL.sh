75564abeacf9ac3b5ad278eba56261a815002ce8
sm <sorooshm78@gmail.com>
2020-11-22 20:13:33 +0330

    add git and vim config

---
+++ INSTALL.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt -y install git
+sudo apt -y install htop
+sudo apt -y install vim
+sudo apt -y install g++
+sudo apt -y install gcc
---
+++ SETUP_CONFIG.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod u+x .setup_git.sh  .setup_vim.sh
+
+./.setup_vim.sh
+./.setup_git.sh
