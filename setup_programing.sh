3c1b5a428e186f561d7183d59a88785a61ff2132
sorooshm78@gmail.com <sorooshm78@gmail.com>
2020-12-06 14:16:02 +0330

    setup programing

---
+++ setup_programing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~/Desktop
+
+mkdir work
+cd work/
+
+# linux config
+#git clone https://github.com/sorooshm78/linux_confige.git
+
+# somredis 
+git clone https://github.com/sorooshm78/somredis.git
+
+# snake_game
+git clone https://github.com/sorooshm78/snake_game.git
+
+# uni
+git clone https://github.com/sorooshm78/uni.git
